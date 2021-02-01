3b013517f439ad8d86c743966a895ee12e03455e
kyc <kangycn@gmail.com>
2021-02-01 01:37:26 -0500

    nodejs hello_world

---
+++ nodejs/hello_world/hello_world.js
@@ -0,0 +1,18 @@
+/*
+ * Run "node hello_world.js" from the command line
+ * Visit 127.0.0.1:8000 in the browser
+ */
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
